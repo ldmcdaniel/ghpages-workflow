ae278fd85878590f3f6e3db08ba0b793a533012c
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-05-13 13:54:55 -0500

    further mods

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf public
-mkdir public
+cp -r src public
 
 # compile jade to html
 ./node_modules/.bin/jade src -P
@@ -19,3 +19,10 @@ rm -rf src/*.html \
 
 # Convert ES6 to ES5
 ./node_modules/.bin/babel src --out-dir public -s inline
+
+# clean unneeded files
+rm -rf public/_styles \
+		public/*.jade\
+		public/**/*.jade\
+		public/*.scss \
+		public/**/*.scss
